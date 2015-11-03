a1373616e08aff8d981a624259d47ead14cb4673
moshthepitt <kelvin@jayanoris.com>
2015-11-03 11:42:29 +0300

    add PathAndRename

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,10 @@
+import uuid
+from datetime import datetime
+import os
+
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.utils.deconstruct import deconstructible
 
 
 def fake_users(number=10):
@@ -15,3 +21,26 @@ def fake_users(number=10):
                          )
         User.objects.create_user(**user_data)
     return "%s users created!" % number
+
+
+@deconstructible
+class PathAndRename(object):
+    """
+    Elegant 'upload_to' for images
+    """
+
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        # set filename
+        if hasattr(instance, 'name') and instance.name:
+            filename = '{}.{}'.format(slugify(instance.name[:255]), ext)
+        elif hasattr(instance, 'title') and instance.title:
+            filename = '{}.{}'.format(slugify(instance.title[:255]), ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(slugify(str(uuid.uuid4())), ext)
+        # return the whole path to the file
+        return os.path.join(self.path, "%s" % datetime.now().year, "%s" % datetime.now().month, filename)
